06d47c3b8574411386f4e4338d81edd21e866b0b
GarotoLinux <garotolinux@gmail.com>
2026-06-23 18:06:54 -0300

    Atendente janela

--- demo/src/main/java/com/implementacoes/Objetos/Atendente.java
+++ demo/src/main/java/com/implementacoes/Objetos/Atendente.java
@@ -4,8 +4,8 @@ import java.time.LocalDate;
 
 public class Atendente  extends Funcionario{
 
-    public Atendente(String nome, String senha, String email, float salario, String cargo, LocalDate data_de_admissao) {
-        super(nome, senha, email, salario, cargo, data_de_admissao);
+    public Atendente(String nome, String senha, String email, float salario, String cargo, LocalDate data_de_admissao, String chefe) {
+        super(nome, senha, email, salario, cargo, data_de_admissao, chefe);
     }
 
     
