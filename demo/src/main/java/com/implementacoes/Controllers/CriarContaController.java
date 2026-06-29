e1db9f44d47fde51ff4ca66b78c93aa032b8e0f9
GarotoLinux <garotolinux@gmail.com>
2026-06-29 19:15:13 -0300

    criarConta

---
+++ demo/src/main/java/com/implementacoes/Controllers/CriarContaController.java
@@ -0,0 +1,71 @@
+package com.implementacoes.Controllers;
+
+import java.io.IOException;
+
+import com.implementacoes.Objetos.Empreendedor;
+import com.implementacoes.Objetos.Gerenciador;
+import com.implementacoes.Objetos.Usuario;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+public class CriarContaController {
+
+    public CriarContaController() {
+        
+    }
+    @FXML
+    private TextField EmailField;
+
+    @FXML
+    private TextField EmpresaField;
+
+    @FXML
+    private TextField NomeField;
+
+    @FXML
+    private TextField SenhaField;
+
+    @FXML
+    public boolean Confirmar(ActionEvent event) {
+        try {
+        Gerenciador.adicionar(new Empreendedor(NomeField.getText(), SenhaField.getText(), EmailField.getText(), EmpresaField.getText()));
+        return true;
+    } catch (Exception e) {
+            System.out.println("Deu erro na criação de conta");
+            return false;
+        }
+    }
+
+    public void start() {
+        try {
+            java.net.URL fxmUrl = com.implementacoes.App.class.getResource("/com/implementacoes/CriarContaJan.fxml");
+            FXMLLoader loader = new FXMLLoader(fxmUrl);
+            Parent root = loader.load();
+
+            /* 
+            // CAPTURA O CONTROLLER REAL CRIADO PELO FXML
+            EdiDeltProdutos controllerReal = loader.getController();
+            
+            // Injeta o usuário na instância real da tela e preenche o menu
+            controllerReal.initUsuario(user);
+            */
+
+            Scene cenaDelEditProdutos = new Scene(root);
+            Stage JanelaDelEditProdutos = new Stage();
+            JanelaDelEditProdutos.setTitle("Criar Conta");
+            JanelaDelEditProdutos.setScene(cenaDelEditProdutos);           
+            JanelaDelEditProdutos.show();
+
+        } catch (IOException ex) {
+            ex.printStackTrace();
+            System.out.println("Erro ao carregar o arquivo FXML. Verifique o caminho.");
+        }
+    }
+
+}
